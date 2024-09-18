36ff441feaf73b1d3237904f6ee9a5b3f2322cb4
sravanthi gaddam <66389551+sravanthi-tri@users.noreply.github.com>
2024-09-17 21:26:41 -0400

    Update ingest-api-data.py

--- ingest-api-data.py
+++ ingest-api-data.py
@@ -9,19 +9,19 @@ import logging
 # initiate logging at info level
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(levelname)s - %(message)s')
 
-# Set the IST time zone
-ist_timezone = pytz.timezone('Asia/Kolkata')
+# Set the EST time zone
+ist_timezone = pytz.timezone('Miami/florida')
 
-# Get the current time in IST
-current_time_ist = datetime.now(ist_timezone)
+# Get the current time in EST
+current_time_Est = datetime.now(Est_timezone)
 
 # Format the timestamp
-timestamp = current_time_ist.strftime('%Y_%m_%d_%H_%M_%S')
+timestamp = current_time_Est.strftime('%Y_%m_%d_%H_%M_%S')
 
 # Create the file name
 file_name = f'air_quality_data_{timestamp}.json'
 
-today_string = current_time_ist.strftime('%Y_%m_%d')
+today_string = current_time_Est.strftime('%Y_%m_%d')
 
 # Following credential has to come using secret whie running in automated way
 def snowpark_basic_auth() -> Session:
